e4845c16e5cd8e5d5820d2d8c756773a42fc1f17
Wesley Luk <wesleyluk@gmail.com>
2017-09-04 16:36:11 -0700

    WIP

---
+++ spec/HandDecoder.spec.js
@@ -0,0 +1,7 @@
+const HandDecoder = require('../lib/HandDecoder');
+describe('HandDecoder', () => {
+    it('should decode the hand', () => {
+        const decoder = new HandDecoder();
+
+    });
+});
