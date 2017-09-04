201d523ee55d20468a46e7505aff7194c217f9b1
Wesley Luk <wesleyluk@gmail.com>
2017-09-03 20:51:39 -0700

    Added hand

---
+++ spec/Hand.spec.js
@@ -0,0 +1,18 @@
+const Hand = require('../lib/Hand');
+
+describe('Hand', () => {
+    it('should create a hand', () => {
+        const hand = new Hand([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+        expect(hand.get(3)).toBe(4);
+    });
+
+    it('should draw tiles', () => {
+        const wall = jasmine.createSpyObj('wall', ['next']);
+        wall.next.and.returnValues(1,2,3,4,5,6);
+
+        const hand = new Hand();
+        hand.draw(wall, 5);
+
+        expect(hand.getTiles()).toEqual([1,2,3,4,5]);
+    });
+});
